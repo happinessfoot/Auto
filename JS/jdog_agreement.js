657797e27a9af87d39653a9e2ff63831ca8bd2b0
happinessfoot <ruslik2014@gmail.com>
2021-08-09 14:34:08 +0500

    Тестовый коммит

--- JS/jdog_agreement.js
+++ JS/jdog_agreement.js
@@ -5,6 +5,7 @@ JunkyardDog.jdog_agreement = (function()
     return {
         onLoad : function(context)
         {
+            //test
             console.log(context);
             let formContext = context.getFormContext();
 
