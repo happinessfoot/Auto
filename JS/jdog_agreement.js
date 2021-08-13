c3573bd549a363b55739968ee616a029a64d2af6
happinessfoot <ruslik2014@gmail.com>
2021-08-13 10:04:55 +0500

    Задание 5. 1,2

--- JS/jdog_agreement.js
+++ JS/jdog_agreement.js
@@ -78,7 +78,10 @@ var JunkyardDog = JunkyardDog || {};
 
 JunkyardDog.jdog_agreement = (function()
 {
+    let canSave = true;
+    let lastErrorMsg = "";
     let filterCreditAuto = "";
+    var loaded = false;
     let fetchXmlCreditid=`<fetch version="1.0" output-format="xml-platform" mapping="logical" distinct="true">
             <entity name="jdog_credit">
               <attribute name="jdog_creditid" />
@@ -137,13 +140,20 @@ JunkyardDog.jdog_agreement = (function()
         
         
     }
-
     let contactOnChange = function(context)
     {
         showTabCredit(context);
 
     }
-    
+    function setFilterCredit(formContext,autoid)
+    {
+        filterCreditAuto = `<filter type="and">
+                                    <condition attribute="jdog_autoid" operator="eq" value="${autoid}" />
+                                </filter>`;
+        fetchXml = fetchXmlCreditid.replace("@filter",filterCreditAuto);
+        ControlManager.addCustomView(formContext,"jdog_creditid","{70E2F896-09E1-4634-AA2D-2DD10D43424D}","jdog_credit","Особое представление",fetchXml,layoutXmlCreditid,true);
+            
+    }
     let autoOnChange = function(context)
     {
         let formContext = context.getFormContext();
@@ -152,15 +162,60 @@ JunkyardDog.jdog_agreement = (function()
         let autoValue = autoAttr.getValue();
         if(autoValue!=null)
         {
-            filterCreditAuto = `<filter type="and">
-                                    <condition attribute="jdog_autoid" operator="eq" value="${autoValue[0].id}" />
-                                </filter>`;
-            fetchXml = fetchXmlCreditid.replace("@filter",filterCreditAuto);
-            ControlManager.addCustomView(formContext,"jdog_creditid","{70E2F896-09E1-4634-AA2D-2DD10D43424D}","jdog_credit","Особое представление",fetchXml,layoutXmlCreditid,true);
-            
+            setFilterCredit(formContext,autoValue[0].id);
         }
     }
-    
+    let onSave = function(context)
+    {
+        let saveEvent = context.getEventArgs();
+        let formContext = context.getFormContext();
+        if(!canSave)
+        {
+            alert(`Сохранение прервано!\n${lastErrorMsg}`)
+            console.log(saveEvent);
+            saveEvent.preventDefault();
+        }
+        
+    }
+    function checkingAgreementDate(formContext)
+    {
+        let creditAttr= formContext.getAttribute("jdog_creditid");
+        let creditDateAttr = formContext.getAttribute("jdog_date");
+        if(creditDateAttr.getValue()!=null && creditAttr.getValue()!=null)
+        {
+            let creditDateValue = creditDateAttr.getValue();
+            let creditValue = creditAttr.getValue();
+            let creditPromise = Xrm.WebApi.retrieveRecord("jdog_credit",creditValue[0].id,`?$select=jdog_creditperiod,jdog_datestart,jdog_dateend`);
+            let creditResult = creditPromise.then(function(creditResult){
+                console.log("credit promise:"+creditResult);
+                canSave = creditDateValue>=new Date(creditResult.jdog_datestart) && creditDateValue<=new Date(creditResult.jdog_dateend);
+                console.log("CANSAVE:"+canSave);
+                console.log("creditDateValue:"+creditDateValue);
+                console.log("creditResult.jdog_datestart:"+creditResult.jdog_datestart);
+                if(!canSave)
+                {
+                    lastErrorMsg=`Дата договора не входит в кредитную программу!\nДата начала:${creditResult.jdog_datestart}\nДата окончания:${creditResult.jdog_dateend}`;
+                }else
+                {
+                    console.log("JDOG_CREDITPERIOD")
+                    formContext.getAttribute("jdog_creditperiod").setValue(creditResult.jdog_creditperiod);
+                }
+            },
+            function(error)
+            {
+                console.error("CreditPromise error:"+error.message);
+            });
+        }else if(creditDateAttr.getValue()!=null && creditAttr.getValue()==null)
+        {
+            canSave = true;
+        }
+    }
+    let dateOnChange = function(context)
+    {
+        let formContext = context.getFormContext();
+        checkingAgreementDate(formContext);
+        console.log("Date change");
+    }
     let creditidOnChange = function(context)
     {
         let formContext = context.getFormContext();
@@ -168,16 +223,19 @@ JunkyardDog.jdog_agreement = (function()
         if(creditAttr!=null)
         {
             console.log("creditattr:"+creditAttr);
-            if(creditAttr.getValue()!=null)
+            let creditValue = creditAttr.getValue()
+            if(creditValue!=null)
             {
                 disableCalcCredControls(formContext,false);
+                
             }else
             {
                 disableCalcCredControls(formContext,true);
-    
-            }
-            
+
+            }   
         }
+        checkingAgreementDate(formContext);
+        
     }
     let agreementNameOnChange = function(context)
     {
@@ -195,25 +253,40 @@ JunkyardDog.jdog_agreement = (function()
     return {
         onLoad : function(context)
         {
-            console.log(context);
-            let formContext = context.getFormContext();
-            TabManager.setVisible(formContext,"tabCredit",false);
-            ControlManager.setVisible(formContext,"jdog_summa",false);
-            ControlManager.setVisible(formContext,"jdog_fact",false);
-            ControlManager.setDisabled(formContext,"jdog_creditid",true);
-            disableCalcCredControls(formContext,true);
-            let contactAttr = formContext.getAttribute("jdog_contact");
-            let autoAttr = formContext.getAttribute("jdog_auto");
-            let creditAttr = formContext.getAttribute("jdog_creditid");
-            let agreementNameAttr = formContext.getAttribute("jdog_name")//.replace(/[^\d.-]/g, '')
-            ControlManager.addCustomView(formContext,"jdog_creditid","{70E2F896-09E1-4634-AA2D-2DD10D43424D}","jdog_credit","Особое представление",fetchXmlCreditid,layoutXmlCreditid,true);
-            filterCreditAuto = `<filter type="and">
-                                    <condition attribute="jdog_autoid" operator="eq" value="{831F7011-C8F9-EB11-94EF-000D3A29AC6D}" />
-                                </filter>`;
-            agreementNameAttr.addOnChange(agreementNameOnChange);
-            contactAttr.addOnChange(contactOnChange);
-            autoAttr.addOnChange(autoOnChange);
-            creditAttr.addOnChange(creditidOnChange);
+            if(!loaded)
+            {
+                console.log("ONLOAD");
+                loaded=true;
+                let formContext = context.getFormContext();
+                let contactAttr = formContext.getAttribute("jdog_contact");
+                let autoAttr = formContext.getAttribute("jdog_auto");
+                let creditAttr = formContext.getAttribute("jdog_creditid");
+                let agreementNameAttr = formContext.getAttribute("jdog_name");
+                let dateAttr = formContext.getAttribute("jdog_date");
+                ControlManager.setVisible(formContext,"jdog_summa",false);
+                ControlManager.setVisible(formContext,"jdog_fact",false);
+                if(creditAttr.getValue()==null)
+                {
+                    disableCalcCredControls(formContext,true);
+                    
+                }
+                if(autoAttr.getValue()==null || contactAttr.getValue()==null)
+                {
+                    ControlManager.setDisabled(formContext,"jdog_creditid",true);
+                    TabManager.setVisible(formContext,"tabCredit",false);
+                }
+                if(autoAttr.getValue()!=null)
+                {
+                    setFilterCredit(formContext,autoAttr.getValue()[0].id)
+                }
+                agreementNameAttr.addOnChange(agreementNameOnChange);
+                contactAttr.addOnChange(contactOnChange);
+                autoAttr.addOnChange(autoOnChange);
+                creditAttr.addOnChange(creditidOnChange);
+                formContext.data.entity.addOnSave(onSave);
+                dateAttr.addOnChange(dateOnChange);
+            }
+            //Так как у нас еще есть редактирование записи, мы вызываем эту функцию для проверки внутренних данных
         }
     }
 })();
